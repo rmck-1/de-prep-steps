99c03aaca477f878f443a7de95f2fe4036ac505c
Ross Mckaig <rossmckaig@Mac.lan>
2026-08-05 13:20:06 +0100

    completed function challenges 1

--- 05_functions_and_execution/01_challenges.py
+++ 05_functions_and_execution/01_challenges.py
@@ -13,29 +13,29 @@ from test_api.checks import run_test, skip_test
 def test_multi_type_list():
     multi_type_list = ["I am a string", 42, True, [1, 2, 3]]
 
-    assert type(multi_type_list[0]) == FILL_ME_IN
-    assert type(multi_type_list[1]) == FILL_ME_IN
-    assert type(multi_type_list[2]) == FILL_ME_IN
-    assert type(multi_type_list[3]) == FILL_ME_IN
+    assert type(multi_type_list[0]) == str
+    assert type(multi_type_list[1]) == int
+    assert type(multi_type_list[2]) == bool
+    assert type(multi_type_list[3]) == list
 
 
 # Challenge 1
-@skip_test
+@run_test
 def test_list_mutation():
     letters = ["a", "b", "c"]
     letters.append("d")
     letters.append("g")
 
-    assert letters == FILL_ME_IN
+    assert letters == ["a", "b", "c", "d", "g"]
 
     last_letter = letters.pop()
 
-    assert last_letter == FILL_ME_IN
-    assert letters == FILL_ME_IN
+    assert last_letter == "g"
+    assert letters == ["a", "b", "c", "d"]
 
 
 # Challenge 2
-@skip_test
+@run_test
 def test_nested_lists():
     rows = [
         ["a", "b", "c"],
@@ -43,21 +43,21 @@ def test_nested_lists():
         ["g", "h", "i"],
     ]
 
-    assert rows[0] == FILL_ME_IN
-    assert rows[1] == FILL_ME_IN
-    assert rows[2] == FILL_ME_IN
+    assert rows[0] == ["a", "b", "c"]
+    assert rows[1] == ["d", "e", "f"]
+    assert rows[2] == ["g", "h", "i"]
 
     first_row = rows[0]
     assert first_row[0] == "a"
-    assert first_row[FILL_ME_IN] == "b"
+    assert first_row[1] == "b"
 
-    assert rows[1][FILL_ME_IN] == "e"
-    assert rows[2][FILL_ME_IN] == "g"
-    assert rows[0][FILL_ME_IN] == "c"
+    assert rows[1][1] == "e"
+    assert rows[2][0] == "g"
+    assert rows[0][2] == "c"
 
 
 # Challenge 3
-@skip_test
+@run_test
 def test_dictionary_keys():
     father = {
         "first_name": "Michael",
@@ -66,13 +66,13 @@ def test_dictionary_keys():
     }
 
     key = "first_name"
-    assert father["last_name"] == FILL_ME_IN
-    assert father["age"] == FILL_ME_IN
-    assert father[key] == FILL_ME_IN
+    assert father["last_name"] == "Bluth"
+    assert father["age"] == 33
+    assert father[key] == "Michael"
 
 
 # Challenge 4
-@skip_test
+@run_test
 def test_removing_dict_keys():
     brother_in_law = {
         "name": "Tobias",
@@ -80,14 +80,17 @@ def test_removing_dict_keys():
         "job": "therapist",
     }
 
-    assert brother_in_law["job"] == FILL_ME_IN
+    assert brother_in_law["job"] == "therapist"
 
     del brother_in_law["job"]
-    assert brother_in_law == FILL_ME_IN
+    assert brother_in_law == {
+        "name": "Tobias",
+        "lastname": "Funke",
+                              }
 
 
 # Challenge 5
-@skip_test
+@run_test
 def test_nested_dictionaries():
     bluth_family = {
         "father": {
@@ -100,9 +103,9 @@ def test_nested_dictionaries():
         "daughters": [{"name": "Lindsay"}],
     }
 
-    assert bluth_family["father"]["name"] == FILL_ME_IN
-    assert bluth_family["mother"]["name"] == FILL_ME_IN
-    assert bluth_family["daughters"][FILL_ME_IN][FILL_ME_IN] == "Lindsay"
+    assert bluth_family["father"]["name"] == "George"
+    assert bluth_family["mother"]["name"] == "Lucille"
+    assert bluth_family["daughters"][0]["name"] == "Lindsay"
 
 
 # DO NOT CHANGE CODE BELOW THIS LINE
